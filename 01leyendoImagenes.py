4f79a30b7e89049e5281ec5adf0b5b4106fbb0da
Marcos Barraza <marcos.barraza@gmail.com>
2019-04-22 11:16:09 -0600

    Delete 01leyendoImagenes.py

--- 01leyendoImagenes.py
+++
@@ -1,4 +0,0 @@
-import cv2 # Se carga la libreria cv2
-img = cv2.imread('./images/inteligenciaArtificial.jpg') #  se lee la imagen
-cv2.imshow('Imagen', img) # se despliega la imagen
-cv2.waitKey(1)
