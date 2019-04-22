d677f0cd7d975e38a476891a433d2aedf5bcc61a
Marcos Barraza <marcos.barraza@gmail.com>
2019-04-22 11:16:22 -0600

    Delete 02guardandoImagenes.py

--- 02guardandoImagenes.py
+++
@@ -1,7 +0,0 @@
-import cv2
-#  convierte la imagen a gris
-imagenGris = cv2.imread('images/inteligenciaArtificial.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('Grayscale', imagenGris)
-cv2.imwrite('images/output.jpg', imagenGris) # la graba en gris
-
-cv2.waitKey()
